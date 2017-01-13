5c06aa0fb5ae22f8d04f72d607788936ebadc36a
Mohammad Annas Al Hariri <annas.elh@gmail.com>
2017-01-13 16:31:08 +0700

    Login page update

--- README.md
+++ README.md
@@ -5,7 +5,29 @@ Yii2adminlte ver. 2.0.1
 <b>How to Install</b>
 
 For yii2 basic
-Donwload and extract files to your yii2 root project and let it merge the directories
+
+1. Donwload and extract files to your yii2 root project and let it merge the directories
+2. Add this code in array in the config file
+	return[
+	    ...........
+
+		'aliases' => [
+			'@adminlte/widgets'=>'@vendor/adminlte/yii2-widgets'
+	    	],
+	    ...........
+	]
+3. Add $this->layout = 'login'; in the top of actionLogin function inside SiteController 
 
 For yii2 advanced
-Download and extract files to your yii2 root project, let it merge. Move assets and views directory to tier you want adminlte install
+1. Download and extract files to your yii2 root project, let it merge. Move assets and views directory to tier you want adminlte install
+2. Add this code in array in the backend config file
+	return[
+	    ...........
+
+		'aliases' => [
+			'@adminlte/widgets'=>'@vendor/adminlte/yii2-widgets'
+	    	],
+	    ...........
+	]
+
+3. Add $this->layout = 'login'; in the top of actionLogin function inside SiteController 
---
+++ views/site/login.php
@@ -0,0 +1,50 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+$this->title = 'Login';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-login">
+    <div class="row">
+        <div class="login-box">
+            <div class="col-md-12 box box-radius">
+            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+
+                <?= $form->field($model, 'username', ['template' => '
+                        <div class="col-sm-12" style="margin-top:15px;">
+                            <div class="input-group col-sm-12">
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-user"></span>
+                                </span>
+                                {input}
+                            </div>{error}{hint}
+                        </div>'])->textInput(['autofocus' => true])
+                                ->input('text', ['placeholder'=>'Username']) ?>
+
+                <?= $form->field($model, 'password', ['template' => '
+                        <div class="col-sm-12" style="margin-top:15px;">
+                            <div class="input-group col-sm-12">
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-lock"></span>
+                                </span>
+                                {input}
+                            </div>{error}{hint}
+                        </div>'])->passwordInput()
+                                ->input('password', ['placeholder'=>'Password'])?>
+
+                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+
+                <div class="form-group">
+                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                </div>
+
+            <?php ActiveForm::end(); ?>
+            </div>
+        </div>
+    </div>
+</div>
